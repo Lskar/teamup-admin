03b54263db8a887f3a8853fec4bbe4b076f13679
Ljw <ljw3436498954@qq.com>
2025-10-31 23:03:44 +0800

    返回格式修改

--- teamup-pojo/src/main/java/com/irum/teamup/query/ProjectPageQuery.java
+++ teamup-pojo/src/main/java/com/irum/teamup/query/ProjectPageQuery.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -65,13 +66,16 @@ public class ProjectPageQuery extends PageQuery{
     /**
      * 项目开始时间
      */
+
     @ApiModelProperty("项目开始时间")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date startTime;
 
     /**
      * 项目结束时间
      */
     @ApiModelProperty("项目结束时间")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date endTime;
 
 }
