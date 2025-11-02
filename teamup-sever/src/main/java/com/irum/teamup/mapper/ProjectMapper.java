bb3f012dce1bea574c03235d2ff798f501c9a3f0
distance <1760852464@qq.com>
2025-11-02 13:51:47 +0800

    统计统计与分析的三个接口（第三个可能有bug没测出来）

--- teamup-sever/src/main/java/com/irum/teamup/mapper/ProjectMapper.java
+++ teamup-sever/src/main/java/com/irum/teamup/mapper/ProjectMapper.java
@@ -3,8 +3,29 @@ package com.irum.teamup.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.irum.teamup.po.ProjectDO;
+import com.irum.teamup.vo.CollegeProjectStatsVO;
+import com.irum.teamup.vo.ProjectCategoryStatsVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface ProjectMapper extends BaseMapper<ProjectDO> {
+    @Select("    SELECT \n" +
+            "        project_type as category,\n" +
+            "        COUNT(*) as count\n" +
+            "    FROM project \n" +
+            "    GROUP BY project_type")
+    List<ProjectCategoryStatsVO> selectProjectCountByCategory();
+
+    @Select("SELECT \n" +
+            "        college,\n" +
+            "        COUNT(*) as projectCount,\n" +
+            "        SUM(current_members) as userCount\n" +
+            "    FROM project \n" +
+            "    WHERE college IS NOT NULL AND college != ''\n" +
+            "    GROUP BY college")
+    List<CollegeProjectStatsVO> selectCollegeProjectStats();
+
 }
